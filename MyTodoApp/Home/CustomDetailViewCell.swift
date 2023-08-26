78b549bcd2448943067e4c743b81a5b80d9d2d60
Daisy Hong <hdaisywd@gmail.com>
2023-08-27 03:36:36 +0900

    checkbox 추가

--- MyTodoApp/Home/CustomDetailViewCell.swift
+++ MyTodoApp/Home/CustomDetailViewCell.swift
@@ -6,27 +6,38 @@ class CustomDetailViewCell: UITableViewCell {
     
     let checkbox = UIButton()
     let task = UILabel()
-    let pin = UIButton()
+    let pin = UIImageView()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        checkbox.setImage(UIImage(systemName: "rectangle"), for: .normal)
-        pin.setImage(UIImage(systemName: "pin"), for: .normal)
+        setCheckBox()
+        cellLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func layout() {
-        self.addSubview(pin)
+    func cellLayout() {
         self.addSubview(checkbox)
         self.addSubview(task)
         
         checkbox.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            checkbox.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
-            checkbox.widthAnchor.constraint(equalToConstant: 50)
-        ])
+        task.translatesAutoresizingMaskIntoConstraints = false
+        
+        checkbox.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
+        checkbox.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        task.leadingAnchor.constraint(equalTo: checkbox.trailingAnchor, constant: 10).isActive = true
+        task.centerYAnchor.constraint(equalTo: checkbox.centerYAnchor).isActive = true
+    }
+    
+    func setCheckBox() {
+        checkbox.setImage(UIImage(systemName: "rectangle"), for: .normal)
+        checkbox.addTarget(self, action: #selector(checkboxAction), for: .touchUpInside)
+    }
+    
+    /* 연결된 db로 체크 정보 보내주기 */
+    @objc func checkboxAction() {
+        
     }
 }
